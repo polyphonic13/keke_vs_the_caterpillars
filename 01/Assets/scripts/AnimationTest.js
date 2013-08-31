c380086f2af27103dcab8f157ff50aba81e5a3ce
paul hoover <paulhhoover@hotmail.com>
2013-08-31 11:41:48 -0400

    begun Slingshot c# script (animation clips not being found though)

---
+++ 01/Assets/scripts/AnimationTest.js
@@ -0,0 +1,59 @@
+﻿public var defaultAnimation: AnimationClip;
+
+public var cockedAnimation: AnimationClip;
+public var fireAnimation: AnimationClip;
+
+private var _animation: Animation;
+
+function Awake () {
+	// _animation = GetComponent(Animation);
+	_animation = this.gameObject.GetComponent(Animation);
+	if(!_animation) {
+		Debug.Log("no animation found for slingshot");
+	}
+	if(!defaultAnimation) {
+		_animation = null;
+	}
+	if(!cockedAnimation) {
+		_animation = null;
+	}
+	if(!fireAnimation) {
+		_animation = null;
+	}
+	
+	var list = GetAnimationNames(_animation);
+	Debug.Log("animations = " + list + ", number = " + list.length);
+	// Debug.Log("AnimationTest/Awake, _animation = " + _animation + ", cocked = " + cockedAnimation + ", fire = " + fireAnimation + ", default = " + defaultAnimation);
+	// _animation[defaultAnimation.name].wrapMode = WrapMode.Once;
+	// _animation.Play(defaultAnimation.name);
+}
+
+function Update () {
+	if(_animation != null) {
+	
+		if(Input.GetKey(KeyCode.G)) {
+			_animation[cockedAnimation.name].wrapMode = WrapMode.Once;
+			_animation.Play(cockedAnimation.name);
+		}
+		if(Input.GetKey(KeyCode.F)) {
+			_animation[fireAnimation.name].wrapMode = WrapMode.Once;
+			_animation.Play(fireAnimation.name);
+		}
+	}
+}
+
+function GetAnimationNames(anim: Animation) {
+	// make an Array that can grow
+	var tmpList : Array = new Array();
+ 
+	// enumerate all states
+	for (var state : AnimationState in anim) {
+		// add name to tmpList
+		//tmpList.Add(state.name);
+		tmpList.push(state.name);
+	}
+	// convert to (faster) buildin array (but can't grow anymore)
+	// var list : string[] = tmpList.ToBuildin(string);
+	// return list;
+	return tmpList;
+}
--- 01/Assets/scripts/Slingshot.cs
+++ 01/Assets/scripts/Slingshot.cs
@@ -4,15 +4,48 @@ using System.Collections;
 public class Slingshot : MonoBehaviour {
 
 	public bool collected = false;
+	public AnimationClip defaultAnimation;
+	public AnimationClip cockedAnimation;
+	public AnimationClip fireAnimation;
 	
+	Animation _animation;
+	bool _hasAnimations = false;
+	bool _canFire = false;
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		_animation = GetComponent<Animation>();
+		if(!defaultAnimation) {
+			_animation = null;
+		}
+		if(!cockedAnimation) {
+			_animation = null;
+		}
+		if(!fireAnimation) {
+			_animation = null;
+		}
+		if(_animation) {
+			_hasAnimations = true;
+		}
+		Debug.Log("Slingshot/Awake, _hasAnimations = " + _hasAnimations + ", _animation = " + _animation + ", _animation[cocked] = " + cockedAnimation.name);
+		_animation.Play(defaultAnimation.name);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(collected && _hasAnimations) {
+			if(Input.GetKey(KeyCode.F)) {
+				Debug.Log("Slingshot play cocked, cocked = " + cockedAnimation.name + ", _animation = " + _animation);
+				_animation.Play(cockedAnimation.name);
+				_canFire = true;
+			}
+			if(Input.GetKey(KeyCode.G)) {
+				if(_canFire) {
+					Debug.Log("Slingshot play fire");
+					_animation.Play(fireAnimation.name);
+					_canFire = false;
+				}
+			}
+		}
 	}
 	
 	void OnTriggerEnter(Collider target) {
@@ -23,4 +56,5 @@ public class Slingshot : MonoBehaviour {
 			}
 		}
 	}
+
 }
